5ced605f20025368bf90b2ac01521171a387d574
haova999 <44152454+haova999@users.noreply.github.com>
2020-02-26 21:56:53 +0700

    Updates

---
+++ precache-manifest.578eab340b2158b557830aff52f16158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59344535486dd2441de0ef55032cba9d",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "bbe36cec4033c7c9cc7e",
+    "url": "/my-first-react-app/static/css/main.cbe57f90.chunk.css"
+  },
+  {
+    "revision": "f3d906e0cd5a36f49650",
+    "url": "/my-first-react-app/static/js/2.77c34601.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-first-react-app/static/js/2.77c34601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe36cec4033c7c9cc7e",
+    "url": "/my-first-react-app/static/js/main.4e0dcb39.chunk.js"
+  },
+  {
+    "revision": "635991cedec334720dcf",
+    "url": "/my-first-react-app/static/js/runtime-main.ff4a67e7.js"
+  }
+]);
